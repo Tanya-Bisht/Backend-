959e568787cbda807dd57fe76e92d44c1e63ccdd
tanyasunfox <tanya.bisht@sunfox.in>
2023-08-22 12:31:54 +0530

    Router setup,DB connected and first signup done

--- Routes/productsRouter.js
+++ Routes/productsRouter.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const authorization = require('../Middleware/authorization');
 const router=express.Router();
-
+const Products=require("../Model/Products");
 router.post('/',authorization,(req,res)=>{
     res.send("Products added");
 
